b0b1471940191e2337c0ea0a52a927789c7a02c6
joya 李 <212014472@qq.com>
2025-02-27 22:02:48 +0800

    achieve sdb

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -18,7 +18,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
-
+#include <stdlib.h>
 static int is_batch_mode = false;
 
 void init_regex();
@@ -27,7 +27,7 @@ void init_wp_pool();
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
   static char *line_read = NULL;
-
+  //负责管理输入行的内存，确保不会发生内存泄漏
   if (line_read) {
     free(line_read);
     line_read = NULL;
@@ -41,8 +41,9 @@ static char* rl_gets() {
 
   return line_read;
 }
-
-static int cmd_c(char *args) {
+//cmd_c 继续运行被暂停的程序
+static int cmd_c(char *args) { 
+  //-1 会被解释为无符号整数的最大值
   cpu_exec(-1);
   return 0;
 }
@@ -51,6 +52,41 @@ static int cmd_c(char *args) {
 static int cmd_q(char *args) {
 	nemu_state.state = NEMU_QUIT;
   return -1;
+}
+
+static int cmd_si(char *args) {
+  if(args == NULL) cpu_exec(1);
+  if(!isdigit(args)) panic("si [N]; N is a number");
+  cpu_exec(atoi(args));
+  return 0;
+}
+
+static int cmd_r_w(char *args) {
+  char *arg = strtok(NULL, " ");
+  if(strcmp(arg,"r") == 0){
+    isa_reg_display();
+  }else if(strcmp(arg,"w") == 0){
+    TODO();
+  }else panic("info r/w;");
+  return 0;
+}
+
+static int cmd_x(char *args) {
+  char *arg1 = strtok(NULL," ");
+  char *arg2 = strtok(NULL," ";)
+  word_t offset = (word_t)strtoul(arg2, NULL, 16) - RESET_VECTOR;
+  if (offset % sizeof(pmem[0]) != 0){
+    panic("Error: Address is not aligned!");
+  }
+  pmem_index = offset / sizeof(pmem[0]);
+  if(pmem >= CONFIG_MSIZE){
+    panic("Error: Address out of range!");
+  }
+
+  for(int i=0;i<atoi(arg1);i++){
+    printf("0x%08x : 0x%08x\n",(word_t)strtoul(arg2, NULL, 16)+8*i,pmem[pmem_index]);
+  }
+  return 0;
 }
 
 static int cmd_help(char *args);
@@ -63,7 +99,8 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si","Execute N instructions in a single step. When N is not given, the default is 1",cmd_si},
+  { "info","Print register status or print monitoring point information",cmd_r_w};
   /* TODO: Add more commands */
 
 };
